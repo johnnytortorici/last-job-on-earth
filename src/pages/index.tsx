c7c8cd00fec5b6592bc2a48aac4123ff8c75eaae
Johnny <johnny.tortorici@gmail.com>
2023-03-30 12:30:35 -0400

    Install tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -17,9 +16,11 @@ export default function Home() {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌎</text></svg>"
         />
       </Head>
-      <main className={styles.main}>
-        <h1>The last job on earth 🌎</h1>
-        <h2>2023 Edition</h2>
+      <main className="container mx-auto py-16 px-4">
+        <h1 className="text-4xl text-center font-bold mb-5">
+          The last job on earth 🌎
+        </h1>
+        <h2 className="text-2xl text-center mb-5">2023 Edition</h2>
         <h3>
           Our best guess of what the last human occupation will be once
           AI-powered robots take over the world...
